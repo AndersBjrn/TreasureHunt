bd2be83a1130cfa4374030f73ba63cd582483b3b
STENUNGSUND\anders.bjorn <anders.bjorn@outlook.com>
2017-10-09 10:34:12 +0200

    First commit

---
+++ TreasureHunt/TreasureHunt/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TreasureHunt.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TreasureHunt/TreasureHunt/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TreasureHunt.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
