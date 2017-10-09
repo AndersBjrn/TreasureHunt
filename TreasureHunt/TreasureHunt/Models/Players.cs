d658f3da0e5cda21c5f569e9406638aada4bc2bc
STENUNGSUND\lena.fridlund <lena.fridlund@hotmail.com>
2017-10-09 11:12:01 +0200

    Kommenterat bort stycke i App_Start. Skapat ny klass "Player".

---
+++ TreasureHunt/TreasureHunt/Models/Players.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreasureHunt.Models
+{
+    public class Players
+    {
+        Riddles = new List<Riddle>();
+        Highscores = new List<Highscore>();
+
+    }
+
+    public virtual Guid PlayerID { get; set; }
+    public virtual string PlayerName { get; set; }
+    public virtual string PlayerEmail { get; set; }    
+    
+    public virtual ICollection<Riddle> Riddles { get; set; }
+    public virtual ICollection<Highscore> Highscores { get; set; }
+}
