70a7c400d6de18b12f24bdd7caad654b17a67582
STENUNGSUND\anders.bjorn <anders.bjorn@outlook.com>
2017-10-09 10:44:42 +0200

    Installed nHibernate. Added DBservice

---
+++ TreasureHunt/TreasureHunt/Services/DBService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreasureHunt.Services
+{
+    public class DBService
+    {
+        public static Configuration Configure()
+        {
+            Configuration cfg = new Configuration()
+                           .DataBaseIntegration(db =>
+                           {
+                               db.ConnectionString = @"Server = (localdb)\mssqllocaldb; Database = treasurehunt; Trusted_Connection = True;";
+                               db.Dialect<MsSql2008Dialect>();
+                           });
+
+            var mapper = new ModelMapper();
+            Type[] myTypes = Assembly.GetExecutingAssembly().GetExportedTypes();
+            mapper.AddMappings(myTypes);
+
+            HbmMapping mapping = new NHibernateMapper().Map();
+            cfg.AddMapping(mapping);
+
+            return cfg;
+        }
+
+        public static ISession OpenSession()
+        {
+            if (_sessionFactory == null)
+            {
+                _sessionFactory = Configure().BuildSessionFactory();
+            }
+            ISession session = _sessionFactory.OpenSession();
+            session.BeginTransaction();
+            return session;
+        }
+
+        public static void CloseSession(ISession session)
+        {
+            if (session != null)
+            {
+                if (session.IsOpen)
+                {
+                    session.Transaction.Commit();
+                    session.Close();
+                }
+            }
+        }
+
+        private static ISessionFactory _sessionFactory;
+
+    }
+}
