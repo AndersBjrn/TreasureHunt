1612a62699ba5effc4a65c9bee3fac9ce5faeb43
STENUNGSUND\lena.fridlund <lena.fridlund@hotmail.com>
2017-10-11 10:24:23 +0200

    Börjat med med loginfunktion.

--- treasurehuntAurelia/src/login.js
+++ treasurehuntAurelia/src/login.js
@@ -1,3 +1,30 @@
 export class Login {
 
+    login() {
+        //$("#loginButton").click(function () {
+
+        //    var username = $("#playername").val()
+        //    var password = $("#password").val()
+
+        //    $("#response").html("")
+        //    $("#error").html("")
+
+        //    $.ajax({
+        //        url: '/session/login',
+        //        method: 'GET',
+        //        data: {
+        //            playerName: playername,
+        //            playerPassword: password
+        //        }
+        //    })
+        //        .done(function (response) {
+        //            $("#response").html(`Log in status: ${response}`);
+        //            window.location.replace(`/home/mymovies?username=${playername}`)
+        //        })
+        //        .fail(function (xhr, status, error) {
+        //            console.log("Error", xhr, status, error)
+        //            $("#error").html(`Error! ${xhr.responseJSON.Message}`);
+                //})
+        //})
+    }
 }
